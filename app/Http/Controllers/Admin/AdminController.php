18681d862af74226430e5a7503081a46cc0912e2
Topu492 <topu86702@gmail.com>
2025-08-18 20:22:33 +0600

    Admin Reset Password with Mailtrap  Successful

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Mail\Websitemail;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -43,6 +46,63 @@ class AdminController extends Controller
     {
         Auth::guard('admin')->logout();
         return redirect()->route('admin_login')->with('success', 'Logged out successfully');
+    }
+
+     public function forget_password()
+    {
+        return view('admin.forget_password');
+    }
+
+    public function forget_password_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $admin = Admin::where('email', $request->email)->first();
+        if(!$admin){
+            return redirect()->back()->with('error', 'Email not found');
+        }
+
+        $token = hash('sha256', time());
+        $admin->token = $token;
+        $admin->update();
+
+        $link = route('admin_reset_password', [$token,$request->email]);
+        $subject = 'Reset Password';
+        $message = 'Click on the following link to reset your password: <br>';
+        $message .= '<a href="'.$link.'">'.$link.'</a>';
+
+        \Mail::to($request->email)->send(new Websitemail($subject,$message));
+
+        return redirect()->back()->with('success', 'Reset password link sent to your email');
+
+    }
+
+
+    public function reset_password($token, $email)
+    {
+        $admin = Admin::where('email', $email)->where('token', $token)->first();
+        if(!$admin){
+            return redirect()->route('admin_login')->with('error', 'Invalid token or email');
+        }
+        return view('admin.reset_password', compact('token', 'email'));
+    }
+
+
+     public function reset_password_submit(Request $request, $token, $email)
+    {
+        $request->validate([
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        $admin = Admin::where('email', $email)->where('token', $token)->first();
+        $admin->password = Hash::make($request->password);
+        $admin->token = '';
+        $admin->update();
+
+        return redirect()->route('admin_login')->with('success', 'Password reset successfully');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,13 @@ Route::middleware('admin')->prefix('admin')->group(function(){
 
 Route::prefix('admin')->group(function(){
    
+   Route::get('/', function () {return redirect()->route('admin_login');});
     Route::get('/login', [AdminController::class, 'login'])->name('admin_login');
     Route::post('/login', [AdminController::class, 'login_submit'])->name('admin_login_submit');
+    Route::get('/forget-password', [AdminController::class, 'forget_password'])->name('admin_forget_password');
+    Route::post('/forget-password', [AdminController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
+    Route::get('/reset-password/{token}/{email}', [AdminController::class, 'reset_password'])->name('admin_reset_password');
+    Route::post('/reset-password/{token}/{email}', [AdminController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('admin_logout');
     
 });
